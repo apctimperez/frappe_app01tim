772a290e990814312dcee75481e0d0a072c5ea58
apctimperez <71299474+apctimperez@users.noreply.github.com>
2024-10-10 06:42:42 +0000

    Create New DocType

---
+++ my_app01/my_app01_test/doctype/my_test_doc_type/my_test_doc_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Timothy Louise R. Perez and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("My Test Doc Type", {
+// 	refresh(frm) {
+
+// 	},
+// });
